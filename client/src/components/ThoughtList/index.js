e891f54481012a3fe16e3a57445c2db0c37fbca8
kdingman <kcdonlan@yahoo.com>
2022-04-20 15:29:50 -0400

    fix issues

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { link } from "react-router-dom";
+
 // Instruct the Thoughtlist component to receive 2 props -> Title and Thoughts array
 // Destruct the argument data to avoid using props.title and props.thoughts thoughout JSX code
 // Render JSX by checking to see if theres even any data in the thoughts array first
@@ -18,15 +20,23 @@ return (
             thoughts.map(thought => (
                 <div key={thought.id} className="card mb-3">
                     <p className="card-header">
-                        {thought.username}
+                        <Link 
+                            to={`/profile/${thought.username}`}
+                            style={{ fontWeight: 700 }}
+                            className="text-light"
+                        >
+                            {thought.username}
+                        </Link>{' '}
                         thought on {thought.createdAt}
                     </p>
                     <div className="card-body">
+                        <Link to={`/thought/${thought._id}`}>                    
                         <p>{thought.thoughtText}</p>
                         <p className="mb-0">
                             Reactions: {thought.reactionCount} || Click to{' '}
                             {thought.reactionCount ? 'see' : 'start'} the discussion!
                         </p>
+                        </Link>
                     </div>
                     </div>
             ))}
