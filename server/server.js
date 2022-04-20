50f22a9bb4e821457b02c1aeda92f0041fe01994
kdingman <kcdonlan@yahoo.com>
2022-04-20 13:23:49 -0400

    finished set up of client side

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 // import Apollo
 const { ApolloServer } = require("apollo-server-express");
@@ -33,6 +34,16 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// Wild Card GET route for the server
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
